55c36663d490fb311f8f5944f428bc1aa8277bdf
NyLaurent <laulanyumbayire@gmail.com>
2024-10-21 09:05:39 -0600

    onboarding screen finished

--- app/index.tsx
+++ app/index.tsx
@@ -10,10 +10,11 @@ export default function Index() {
         source={require("../assets/images/onBoarding.png")}
         className="w-full h-full  flex-1"
       />
-      <View className="absolute bottom-20 left-10   space-y-6">
-        <Text className="text-5xl font-semibold text-white">SpaceCrafts</Text>
-        <Text className="font-semibold text-white text-lg text-center ">
-          learn more details about the whole planets in the universe
+
+      <View className="absolute bottom-20 left-1 space-y-6">
+        <Text className="text-5xl font-semibold text-white text-center">SpaceCrafts</Text>
+        <Text className="font-semibold text-white text-lg text-center px-4 ">
+          Learn more details about the whole planets in the universe
         </Text>
         <TouchableOpacity className="bg-black text-white bg-opacity-10 justify-center items-center py-3 rounded-2xl" onPress={() => router.navigate('/(tabs)')}>
           <Text className="text-white text-2xl font-semibold">Get started</Text>
